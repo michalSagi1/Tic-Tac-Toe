bdb9a9fe5fe86e2e57b4eee0c4b2888621d28dde
michal sagi <michaldinner@gmail.com>
2022-05-15 11:15:09 +0300

    board, xo , style

--- index.js
+++ index.js
@@ -1,10 +1,65 @@
 
+const board = document.getElementById("board")
+let num = 3
+let arr = []
+let a = 0
+for (let i = 0; i < num; i++) {
+    arr[i] = []
+    const element = createCard(i)
+    board.appendChild(element)
+
+    for (let j = 0; j < num; j++) {
+        arr[i][j] = `${a}`
+        const element = createCard1(a, i, j)
+        board.appendChild(element)
+        a++
+
+    }
+}
 
 
-const arr= [
-  [  , ,  ],
-  [  , ,  ],
-  [  , ,  ]
+let cnt = 0
 
-];
+function createCard() {
+    const cardEl = document.createElement("div")
+    cardEl.className = `row`
+    return cardEl
+}
+
+function createCard1(a, i, j) {
+    const cardEl = document.createElement("div")
+    cardEl.id = `${a}`
+    cardEl.innerHTML = "❤️"
+    cardEl.className = `cards col-1 col-md-${12 / num}`
+
+
+    cardEl.addEventListener('click', cardEl.fn = function fn() {
+        if (cnt == 0) {
+            cardEl.innerHTML = "❌"
+            cnt++
+            cardEl.removeEventListener('click', cardEl.fn, false)
+            arr[i][j] = 'x'
+            let b = 0
+
+
+
+        }
+        else {
+            cardEl.innerHTML = "⭕"
+            cnt = 0
+            cardEl.removeEventListener('click', cardEl.fn, false)
+            arr[i][j] = 'o'
+
+        }
+
+
+    })
+
+
+
+
+    return cardEl
+}
+
+
 
